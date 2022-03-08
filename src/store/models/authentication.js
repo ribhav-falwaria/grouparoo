7507abb12402f4f270e333275215814750c5601f
jsaurabh2794-novopay <saurabh.kumar@novopay.in>
2022-03-08 15:47:37 +0530

    signIn fix

--- src/store/models/authentication.js
+++ src/store/models/authentication.js
@@ -40,7 +40,13 @@ const authentication = {
         const customer = await apiService.appApi.auth.login(email, password)
         const customerDetails = await apiService.appApi.customer.getCustomerByUserId(customer.$id)
         const loanApplications = await apiService.appApi.loanApplication.getAllLoanApplications(customerDetails.$id)
-        return dispatch.customer.setCustomer({ customer, isFirstTime, customerDetails, loanApplications })
+        //return dispatch.customer.setCustomer({ customer, isFirstTime, customerDetails, loanApplications })
+        Promise.all([
+          dispatch.customer.setCustomer({ customer, isFirstTime, customerDetails, loanApplications }),
+          dispatch.loanProducts.getAllProducts(),
+          dispatch.loanTypes.getAllLoanTypes(),
+          dispatch.borrowingEntities.getBorrowingEntities()
+        ])
       } catch (e) {
         // FIXME: Endpoint to push errors
         return dispatch.appStates.setSigninError({ signinError: true })
