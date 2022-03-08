16eb501f5c823a2ac4956160bc5525d8c449dbd2
jsaurabh2794-novopay <saurabh.kumar@novopay.in>
2022-03-08 13:17:04 +0530

    CIBIL OTP Widget Fix to useRequest

--- src/screens/components/React-json-schema-form/components/extLibraries/JsonSchemaForm/widgets/CIBILOtpWidget.js
+++ src/screens/components/React-json-schema-form/components/extLibraries/JsonSchemaForm/widgets/CIBILOtpWidget.js
@@ -1,28 +1,90 @@
-import React, { useEffect, useState } from 'react'
-import { useToast } from 'react-native-toast-notifications'
-import { useSelector } from 'react-redux'
-import { View, StyleSheet } from 'react-native'
-import isEmpty from 'lodash.isempty'
-import { Text } from '@ui-kitten/components'
-import { useFormContext } from '..'
-import ReactJsonSchemaUtil from '../../../../services/ReactJsonSchemaFormUtil'
-import ResourceFactoryConstants from '../../../../services/ResourceFactoryConstants'
-import MaskedInput from '../../textMask/text-input-mask'
-import DataService from '../../../../services/DataService'
-import LoadingSpinner from '../../../../../../components/LoadingSpinner'
-import OtpComponent from '../../../../../OtpComponent'
-const WAIT_RESEND_MS = 2 * 60 * 1000
-const OTP_VALID_MS = 5 * 60 * 1000
-let transId
+import React, { useContext, useEffect, useState } from "react";
+import { useToast } from "react-native-toast-notifications";
+import { useSelector } from "react-redux";
+import { View, StyleSheet } from "react-native";
+import isEmpty from "lodash.isempty";
+import { Text } from "@ui-kitten/components";
+import { useFormContext } from "..";
+import ReactJsonSchemaUtil from "../../../../services/ReactJsonSchemaFormUtil";
+import ResourceFactoryConstants from "../../../../services/ResourceFactoryConstants";
+import MaskedInput from "../../textMask/text-input-mask";
+import DataService from "../../../../services/DataService";
+import LoadingSpinner from "../../../../../../components/LoadingSpinner";
+import OtpComponent from "../../../../../OtpComponent";
+import { useRequest } from "ahooks";
+import Toast from "react-native-toast-message";
+import crashlytics from "@react-native-firebase/crashlytics";
+import { LocalizationContext } from "../../../../translation/Translation";
+const resourceFactoryConstants = new ResourceFactoryConstants();
+const generateCIBILOtp = async (requestBody) => {
+  try {
+    const response = await DataService.postData(
+      resourceFactoryConstants.constants.cibil.sendOTPForCibil,
+      requestBody
+    );
+    const data = response.data;
+    if (data.status === "SUCCESS") {
+      crashlytics().log("CIBIL OTP Data", data);
+      return data;
+    } else {
+      throw new Error("ERROR_WHILE_OTP_GENERATION");
+    }
+  } catch (error) {
+    crashlytics().recordError(error);
+    if (error.message === "ERROR_WHILE_OTP_GENERATION") {
+      throw err;
+    } else {
+      throw new Error("ERROR_WHILE_REACHING_TO_CIBIL_SERVER");
+    }
+  }
+};
+
+const validateCibilOtp = async (otp, loanAppId) => {
+  crashlytics().log(
+    `Request body while calling ${
+      resourceFactoryConstants.constants.cibil.verifyCibilOtp
+    } is ${{
+      transId: transId,
+      otp: otp,
+      loanApplicationId: loanAppId,
+    }}`
+  );
+  try {
+    const response = await DataService.postData(
+      resourceFactoryConstants.constants.cibil.verifyCibilOtp,
+      {
+        transId: transId,
+        otp: otp,
+        loanApplicationId: loanAppId,
+      }
+    );
+    const data = response.data;
+    console.log("Response Data while otp enter", data);
+    if (data.status === "SUCCESS") {
+      return true;
+    } else {
+      throw new Error("FAILED_WHILE_OTP_VALIDATION");
+    }
+  } catch (error) {
+    if (error.message === "FAILED_WHILE_OTP_VALIDATION") {
+      throw error;
+    } else {
+      throw new Error("ERROR_WHILE_REACHING_TO_CIBIL_SERVER");
+    }
+  }
+};
+
+const WAIT_RESEND_MS = 2 * 60 * 1000;
+const OTP_VALID_MS = 5 * 60 * 1000;
+let transId;
 const CIBILOtpWidget = (props) => {
-  const [otpValid, setOtpValid] = useState('default')
-  const { theme } = useFormContext()
-  const currentFormData = useSelector((state) => state?.formDetails?.formData)
-  const panData = useSelector((state) => state?.formDetails?.panData)
-  const [isFormValid, setIsValid] = useState(true)
-  const [loaderVisibility, setLoaderVisibility] = useState(false)
-  const toast = useToast()
-  const resourceFactoryConstants = new ResourceFactoryConstants()
+  const { translations } = useContext(LocalizationContext);
+  const [otpValid, setOtpValid] = useState("default");
+  const { theme } = useFormContext();
+  const currentFormData = useSelector((state) => state?.formDetails?.formData);
+  const loanAppId = currentFormData?.loanApplicationId;
+  const panData = useSelector((state) => state?.formDetails?.panData);
+  const [isFormValid, setIsValid] = useState(true);
 
   const isRequiredFieldPresent = (currentFormData, panData) => {
     if (
@@ -30,128 +92,140 @@ const CIBILOtpWidget = (props) => {
       isEmpty(currentFormData?.primaryPhone) ||
       isEmpty(panData)
     ) {
-      return false
+      return false;
     }
-    return true
-  }
+    return true;
+  };
+
+  const useGenerateCIBILOtp = useRequest(generateCIBILOtp, {
+    manual: true,
+    onSuccess: (results, params) => {
+      transId = results.transId;
+      Toast.show({
+        type: "success",
+        position: "bottom",
+        props: {
+          title: translations["cibil.title"],
+          description: results.message,
+        },
+      });
+      crashlytics().log("Transaction Id for Cibil", transId);
+    },
+    onError: (error, params) => {
+      if (
+        error.message === "ERROR_WHILE_OTP_GENERATION" ||
+        error.message === "ERROR_WHILE_REACHING_TO_CIBIL_SERVER"
+      ) {
+        throw error;
+      } else {
+        Toast.show({
+          type: "error",
+          position: "bottom",
+          props: {
+            title: translations["cibil.title"],
+            description: translations["cibil.otp.generation.failed"],
+          },
+        });
+      }
+    },
+  });
+
+  const useValidateCIBILOtp = useRequest(validateCibilOtp, {
+    manual: true,
+    onSuccess: (results, params) => {
+      props.onChange("Yes");
+      props.formContext.submit();
+      setOtpValid("valid");
+      Toast.show({
+        type: "success",
+        position: "bottom",
+        props: {
+          title: translations["cibil.title"],
+          description: translations["cibil.success"],
+        },
+      });
+    },
+    onError: (error, params) => {
+      if (error.message === "ERROR_WHILE_REACHING_TO_CIBIL_SERVER") {
+        throw error;
+      } else {
+        Toast.show({
+          type: "error",
+          position: "bottom",
+          props: {
+            title: translations["cibil.title"],
+            description: translations["cibil.otp.verification.failed"],
+          },
+        });
+      }
+    },
+  });
 
   useEffect(() => {
     if (!props.value && isRequiredFieldPresent(currentFormData, panData)) {
-      generateCIBILOtp()
+      useGenerateCIBILOtp.run(getResquestBodyForCIBIL());
     }
-  }, [])
+  }, []);
 
   const getResquestBodyForCIBIL = () => {
-    console.log('Form Data at CIBIL Widget', currentFormData)
+    crashlytics().log("Form Data at CIBIL Widget", currentFormData);
     const names =
-      currentFormData?.fullName?.split(' ') ||
-      panData?.name.trim().split(' ') ||
-      ''
-    const mobileNoWithoutCountryCode = currentFormData?.primaryPhone
+      currentFormData?.fullName?.split(" ") ||
+      panData?.name.trim().split(" ") ||
+      "";
+    const mobileNoWithoutCountryCode = currentFormData?.primaryPhone;
     const payload = {
-      firstName: names.length > 0 ? names[0] : '',
-      middleName: names.length > 2 ? names[1] : '',
+      loanApplicationId: loanAppId,
+      firstName: names.length > 0 ? names[0] : "",
+      middleName: names.length > 2 ? names[1] : "",
       lastName: names.length > 2 ? names[2] : names[1],
       mobileNumber: mobileNoWithoutCountryCode,
-      emailAddress: currentFormData?.email || 'nplending@gmail.com',
+      emailAddress: currentFormData?.email || "nplending@gmail.com",
       dob: ReactJsonSchemaUtil.formatDateToDefaultDate(
         panData?.dob,
-        'DD/MM/YYYY',
-        'DD-MM-YYYY'
+        "DD/MM/YYYY",
+        "DD-MM-YYYY"
       ),
       idNumber: panData?.panNumber,
-      gender: currentFormData?.sex || 'Other',
+      gender: currentFormData?.sex || "Other",
       addressLine1:
-        currentFormData?.residentialAddress?.address2 || 'Bengaluru',
-      addressLine2: currentFormData?.residentialAddress?.address2 || '',
-      city: currentFormData?.residentialAddress?.city || 'Bengaluru',
-      state: currentFormData?.residentialAddress?.state || '29',
+        currentFormData?.residentialAddress?.address2 || "Bengaluru",
+      addressLine2: currentFormData?.residentialAddress?.address2 || "",
+      city: currentFormData?.residentialAddress?.city || "Bengaluru",
+      state: currentFormData?.residentialAddress?.state || "29",
       pinCode:
-        currentFormData?.residentialAddress?.zipCode?.replace(/ /g, '') ||
-        '560103'
-    }
-    console.log('CIBIL Body Data', payload)
-    return payload
-  }
-  const generateCIBILOtp = () => {
-    setLoaderVisibility(true)
-    DataService.postData(
-      resourceFactoryConstants.constants.cibil.sendOTPForCibil,
-      getResquestBodyForCIBIL()
-    )
-      .then((res) => {
-        const data = res.data
-        if (data.status === 'SUCCESS') {
-          toast.show(data.message, { type: 'success' })
-          transId = data.transId
-        } else {
-          toast.show(data.message || 'Unexpected Error occurred', {
-            type: 'error'
-          })
-        }
-        setLoaderVisibility(false)
-      })
-      .catch((err) => {
-        toast.show(err.message, { type: 'danger' })
-        setLoaderVisibility(false)
-      })
-  }
+        currentFormData?.residentialAddress?.zipCode?.replace(/ /g, "") ||
+        "560103",
+    };
+    crashlytics().log("CIBIL Body Data", payload);
+    return payload;
+  };
   useEffect(() => {
-    if (props.required && (!isEmpty(props.rawErrors) || props.value === '')) {
-      setIsValid(false)
+    if (props.required && (!isEmpty(props.rawErrors) || props.value === "")) {
+      setIsValid(false);
     } else {
-      setIsValid(true)
+      setIsValid(true);
     }
-  }, [props.rawErrors, props.value, props.required])
-
-  const onValidateOtp = (otp) => {
-    if (otp.length !== 6) {
-      return
-    }
-    setLoaderVisibility(true)
-    DataService.postData(
-      resourceFactoryConstants.constants.cibil.verifyCibilOtp,
-      {
-        transId: transId,
-        otp: otp
-      }
-    ).then((res) => {
-      const data = res.data
-      if (data.status === 'SUCCESS') {
-        toast.show('CIBIL Score Verified Successfully', { type: 'success' })
-        props.onChange('Yes')
-        props.formContext.submit()
-        setOtpValid('valid')
-      } else {
-        toast.show(data.message, { type: 'danger' })
-        setOtpValid('invalid')
-      }
-      setLoaderVisibility(false)
-    })
-      .catch((err) => {
-        toast.show(err.message, { type: 'danger' })
-        setLoaderVisibility(false)
-      })
-  }
+  }, [props.rawErrors, props.value, props.required]);
 
   return (
     <View style={styles.rowMargin}>
       {!props.value && (
         <OtpComponent
           primaryPhone={currentFormData.primaryPhone}
-          onResendOtp={generateCIBILOtp}
-          loading={loaderVisibility}
-          onValidateOtp={onValidateOtp}
+          onResendOtp={() => useGenerateCIBILOtp.run(getResquestBodyForCIBIL())}
+          loading={useGenerateCIBILOtp.loading || useValidateCIBILOtp.loading}
+          onValidateOtp={(otp) => useValidateCIBILOtp.run(otp, loanAppId)}
           otpValid={otpValid}
           numSecondsWaitForResend={WAIT_RESEND_MS}
           otpValidWindow={OTP_VALID_MS}
         />
       )}
-      {props.value && <Text>CIBIL Score has been verified successfully.</Text>}
+      {props.value && <Text>{translations["cibil.success"]}</Text>}
     </View>
-  )
-}
+  );
+};
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
 
-export default CIBILOtpWidget
+export default CIBILOtpWidget;
